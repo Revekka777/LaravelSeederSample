5baf6fb58ef15dd9820b87bc67d723140de9fa5a
Revekka <zgoda.olya@gmail.com>
2022-10-03 11:35:22 +0300

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(2)->create();
+        for($i = 1; $i < 3; $i++){
+            $post = Post::factory()->create();
+            Comment::factory($i)->post($post)->create();
+        }
+
     }
 }
